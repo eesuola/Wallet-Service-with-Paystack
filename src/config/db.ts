4cd2f03253ee91ec004bd19b598146d9c6c9ab7d
eesuola <eesuolap@gmail.com>
2025-12-10 09:01:18 +0000

    update config file to use render for pg

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,22 +1,35 @@
 import { DataSource } from "typeorm";
-import { User } from "../models/user";           
-import { Wallet } from "../models/wallet";      
-import { Transaction } from "../models/transaction"; 
-import { ApiKey } from "../models/apiKeys";  
+import { User } from "../models/user";
+import { Wallet } from "../models/wallet";
+import { Transaction } from "../models/transaction";
+import { ApiKey } from "../models/apiKeys";
 import dotenv from "dotenv";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 dotenv.config();
 
-export const connectDB = new DataSource({
+let dbConfig: any = {
   type: "postgres",
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || ""),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME,
-  synchronize: true, 
-  logging: false,
+};
+if (process.env.DATABASE_URL) {
+  dbConfig = {
+    ...dbConfig,
+    url: process.env.DATABASE_URL,
+  };
+}
+
+export const connectDB = new DataSource({
+  ...dbConfig,
+  synchronize: !isProduction,
+  logging: !isProduction,
   entities: [User, Wallet, Transaction, ApiKey],
-  subscribers: [],
   migrations: [],
+  subscribers: [],
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
